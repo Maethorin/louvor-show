79acd4af44da36c1ae28674847650e5067b1b5a2
maethorin <maethorin@gmail.com>
2016-02-01 02:41:03 -0200

    Melhorando a renderização de templates do AngularJS

--- app/initialize.py
+++ app/initialize.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 from flask.ext.sqlalchemy import SQLAlchemy
 
 import database
@@ -16,12 +16,12 @@ api.create_api(web_app)
 
 @web_app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return send_from_directory('templates', 'index.html')
 
 
 @web_app.route('/angular/<template_name>', methods=['GET', 'POST'])
 def angular_template(template_name):
-    return web_app.send_static_file("musicas/{}".format(template_name))
+    return send_from_directory('templates', template_name)
 
 
 @web_app.route('/json/<json_name>', methods=['GET', 'POST'])
--- app/routes.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
